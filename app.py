b980358cb52fe39cbfac6b7d7329d0a0a86465f1
Kinshuk Gupta <kinshukg31@gmail.com>
2019-10-11 14:41:03 +0530

    Basic webapp

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, flash, redirect
+from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+import csv
+
+#Importing libraries for the algorithm
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+
+if __name__=="__main__":
+	app.run(debug=True)
+
+

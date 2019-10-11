68f7109eac255e89c87fc1ffb778271fda564806
Monacrylic <siddkot@gmail.com>
2019-10-11 15:18:26 +0530

    Added database

--- app.py
+++ app.py
@@ -8,6 +8,25 @@ import numpy as np
 import pandas as pd
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db= SQLAlchemy(app)
+# use DB-Browser to modify the database
+
+
+class questions_table(db.Model):
+    question_number = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(500), unique=True, nullable=False)
+    #answer type= 'm' for mcq questions, 's' for slider input
+    answer_type = db.Column(db.String(1), nullable=False)
+    #options 1 and 2 can also act as the arguments for slider's min and max values
+    option_1_range= db.Column(db.String(50),nullable=False)
+    option_2_range= db.Column(db.String(50),nullable=False)
+    option_3= db.Column(db.String(50))
+    option_1_value=db.Column(db.Integer,nullable=False)
+    option_2_value=db.Column(db.Integer,nullable=False)
+    option_3_value=db.Column(db.Integer,nullable=False)
+
+
 
 @app.route('/')
 def index():
